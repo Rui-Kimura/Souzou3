5f35ae4b80fd0cad53bb24ba68c41bcfdea4624d
Rui-Kimura <s22114@numazu.kosen-ac.jp>
2025-12-03 14:52:12 +0900

    controller

--- ui/src/app/controller/page.tsx
+++ ui/src/app/controller/page.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import Slider from '@mui/material/Slider';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Fab from '@mui/material/Fab';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
@@ -10,7 +11,7 @@ import Switch from '@mui/material/Switch';
 import { PanelContainer, MasconHandle, ReverserHandle, SemicircleMascon, MechanicalSwitch } from './style';
 import { Typography } from '@mui/material';
 
-const API_HOST = "http://localhost:8100";
+const API_HOST = "/api/local";
 
 export default function ControllerApp() {
     const [masconValue, setMasconValue] = useState(0);
@@ -49,7 +50,6 @@ export default function ControllerApp() {
         sendControlData();
     }, [masconValue, reverserValue, angleValue]);
 
-    /* 手動制御モード設定 */
     useEffect(() => {
         const set_manual_mode = async (mode: boolean) => {
             try {
@@ -74,8 +74,56 @@ export default function ControllerApp() {
         setReverserValue(Array.isArray(newValue) ? newValue[0] : newValue);
     };
 
+    const timerRef = useRef<NodeJS.Timeout | undefined>(undefined);
+
+    const upLinear = async () => {
+        try {
+            await fetch(API_HOST + `/linear?mode=up`, {
+                method: 'GET'
+            });
+        } catch (error) {
+            console.error('Failed to set manual mode:', error);
+        }
+    };
+
+    const downLinear = async () => {
+        try {
+            await fetch(API_HOST + `/linear?mode=down`, {
+                method: 'GET'
+            });
+        } catch (error) {
+            console.error('Failed to set manual mode:', error);
+        }
+    };
+
+    const stopLinear = async () => {
+        try {
+            await fetch(API_HOST + `/linear?mode=stop`, {
+                method: 'GET'
+            });
+        } catch (error) {
+            console.error('Failed to set manual mode:', error);
+        }
+    };
+
+    const up_startPress = () => {
+        upLinear();
+    };
+
+    const up_stopPress = () => {
+        stopLinear();
+    };
+
+    const down_startPress = () => {
+        downLinear();
+    };
+
+    const down_stopPress = () => {
+        stopLinear();
+    };
+
     /*操作画面を閉じるときは停止し制御を切る*/
-    const handleBeforeUnload = async() => {
+    const handleBeforeUnload = async () => {
         setManualMode(false);
         try {
             await fetch(API_HOST + `/set_manual_mode?mode=false`, {
@@ -87,7 +135,7 @@ export default function ControllerApp() {
 
         try {
             await fetch(API_HOST + `/controll_api?direction=0&speed=0&angle=0`, {
-                    method: 'GET'
+                method: 'GET'
             });
         } catch (error) {
             console.error('Failed to send control data:', error);
@@ -101,6 +149,7 @@ export default function ControllerApp() {
             window.removeEventListener('beforeunload', handleBeforeUnload)
         }
     }, [handleBeforeUnload])
+
 
     return (
         <Box sx={{ position: "absolute", top: 0, left: 0, width: "100vw", height: "100vh", backgroundColor: '#222222' }}>
@@ -119,7 +168,7 @@ export default function ControllerApp() {
                         userSelect: 'none',
                     }}
                 >
-                    <Grid width="calc(100%/3)" display={"flex"} height={"100%"}>
+                    <Grid width="calc(100%/3*2 - 10%)" display={"flex"} height={"100%"} sx={{textAlign: "left"}} >
                         <PanelContainer sx={{ mr: 2, marginLeft: 5 }}>
                             <ReverserHandle
                                 value={reverserValue}
@@ -134,11 +183,88 @@ export default function ControllerApp() {
                                 sx={{ marginTop: 2 }}
                             />
                         </PanelContainer>
-                    </Grid>
-
-                    <Grid
-                        width="calc(100%/3 - 10%)"
-                    >
+                        <Box sx={{display:"flex", flexDirection:"column"}}>
+                        <PanelContainer sx={{ display: "flex", justifyContent: "space-around", flexDirection: "column", width: "25%" }}>
+                            <Fab
+                                aria-label="add"
+                                sx={{
+                                    backgroundColor: 'white',
+                                    color: 'Black',
+                                    '&:hover': {
+                                        backgroundColor: '#f5f5f5',
+                                    },
+                                    marginBottom: 1,
+                                    fontSize: '1.2rem'
+                                }}
+                                onMouseDown={up_startPress}  
+                                onMouseUp={up_stopPress}     
+                                onMouseLeave={up_stopPress}  
+                                onTouchStart={up_startPress} 
+                                onTouchEnd={up_stopPress}   
+                            >
+                                上
+                            </Fab>
+                            <Fab
+                                aria-label="add"
+                                sx={{
+                                    backgroundColor: '#B40000',
+                                    color: 'White',
+                                    '&:hover': {
+                                        backgroundColor: '#B40000',
+                                    },
+                                    marginTop: 1,
+                                    fontSize: '1.2rem'
+                                }}
+                                onMouseDown={down_startPress}  
+                                onMouseUp={down_stopPress}     
+                                onMouseLeave={down_stopPress}  
+                                onTouchStart={down_startPress} 
+                                onTouchEnd={down_stopPress}   
+                            >
+                                下
+                            </Fab>
+                        </PanelContainer>
+                        <PanelContainer sx={{ display: "flex", justifyContent: "space-around", flexDirection: "column", width: "25%" }}>
+                            <Fab
+                                aria-label="add"
+                                sx={{
+                                    backgroundColor: 'white',
+                                    color: 'Black',
+                                    '&:hover': {
+                                        backgroundColor: '#f5f5f5',
+                                    },
+                                    marginBottom: 1,
+                                    fontSize: '1.2rem'
+                                }}
+                                onMouseDown={up_startPress}  
+                                onMouseUp={up_stopPress}     
+                                onMouseLeave={up_stopPress}  
+                                onTouchStart={up_startPress} 
+                                onTouchEnd={up_stopPress}   
+                            >
+                                展
+                            </Fab>
+                            <Fab
+                                aria-label="add"
+                                sx={{
+                                    backgroundColor: '#ffffffff',
+                                    color: 'Black',
+                                    '&:hover': {
+                                        backgroundColor: '#B40000',
+                                    },
+                                    marginTop: 1,
+                                    fontSize: '1.2rem'
+                                }}
+                                onMouseDown={down_startPress}  
+                                onMouseUp={down_stopPress}     
+                                onMouseLeave={down_stopPress}  
+                                onTouchStart={down_startPress} 
+                                onTouchEnd={down_stopPress}   
+                            >
+                                収
+                            </Fab>
+                        </PanelContainer>
+                        </Box>
                         {/* 空白 ボタン等入れる予定 */}
                     </Grid>
 
